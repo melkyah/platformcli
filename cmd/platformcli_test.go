f052cd4e90afe3a31ca0dc241d0942180c4db9e9
Francisco Gonzalez <francisco.gonzalez@guerrilla-games.com>
2024-12-30 11:12:20 +0100

    Added Kind creation and deletion tests

--- cmd/platformcli_test.go
+++ cmd/platformcli_test.go
@@ -21,14 +21,6 @@ func Test_CommandErrReturn(t *testing.T) {
 		Subcommand []string
 		WantOut    string
 	}{
-		/*
-			This one is failing. Need to figure out why
-			{
-				Name:       "misspelled subcommand for platform",
-				Command:    "platform",
-				Subcommand: []string{"intall"},
-				WantOut:    "No help topic for 'intall'",
-			},*/
 		{
 			Name:       "unknown flag in platform install command",
 			Command:    "platform",
@@ -56,11 +48,53 @@ func Test_CommandErrReturn(t *testing.T) {
 			t.Parallel()
 			fmt.Printf("Running command %s \n", strings.Join(arguments, " "))
 			err := cmd.Run(context.Background(), arguments)
-			fmt.Printf("%s\n", err.Error())
 			if err == nil {
 				t.Errorf("Subcommand should raise an error if not called with correct params")
 			} else if err.Error() != tc.WantOut {
 				t.Errorf("Error output not matching.\n Expected: %s\n Received: %s", tc.WantOut, err.Error())
+			} else {
+				fmt.Printf("Error:\n%s\n", err.Error())
+			}
+		})
+	}
+}
+
+func Test_CreateDemoCluster(t *testing.T) {
+	cases := []struct {
+		Name       string
+		Command    string
+		Subcommand []string
+	}{
+		{
+			Name:       "create Kind cluster",
+			Command:    "platform",
+			Subcommand: []string{"install", "--demo"},
+		},
+		{
+			Name:       "delete Kind cluster",
+			Command:    "platform",
+			Subcommand: []string{"uninstall", "--demo"},
+		},
+	}
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.Name, func(t *testing.T) {
+			cmd := &cli.Command{
+				Usage:    "A CLI tool to spin up and manage infrastructure platforms",
+				Version:  "v0.0.1",
+				Commands: platform.Commands,
+			}
+			arguments := os.Args[0:1]
+			arguments = slices.Concat(arguments, []string{tc.Command}, tc.Subcommand)
+
+			t.Cleanup(func() {
+				cmd.Run(context.Background(), slices.Concat(os.Args[0:1], []string{"platform", "uninstall", "--demo"}))
+			})
+
+			fmt.Printf("Running command %s \n", strings.Join(arguments, " "))
+			err := cmd.Run(context.Background(), arguments)
+			if err != nil {
+				t.Errorf("Error running Kind operation:\n%s", err.Error())
 			}
 		})
 	}
